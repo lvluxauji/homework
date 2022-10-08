ed2ec4560eac9082eba9500f74200a12ef042455
Mikhail Rumyantsev <m.rumyantsev@codeside.ru>
2022-10-08 20:32:14 +0300

    Add homework for lesson 3

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
+// 🐦 Flutter imports:
 import 'package:flutter/material.dart';
+
+// 📦 Package imports:
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,7 +16,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Homework',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -24,7 +29,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Homework 3'),
     );
   }
 }
@@ -48,19 +53,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     // This method is rerun every time setState is called, for instance as done
@@ -95,21 +87,33 @@ class _MyHomePageState extends State<MyHomePage> {
           // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            CarouselSlider(
+              options: CarouselOptions(
+                height: 240.0,
+                enableInfiniteScroll: true,
+                autoPlay: true,
+                autoPlayCurve: Curves.fastOutSlowIn,
+              ),
+              items: [
+                SvgPicture.asset(
+                  'assets/images/flutter_logo.svg',
+                  semanticsLabel: 'Flutter Logo',
+                ),
+                SvgPicture.network(
+                  'https://raw.githubusercontent.com/dnfield/flutter_svg/'
+                  '7d374d7107561cbd906d7c0ca26fef02cc01e7c8/'
+                  'example/assets/flutter_logo.svg',
+                  semanticsLabel: 'Flutter Logo',
+                  placeholderBuilder: (BuildContext context) => Container(
+                    padding: const EdgeInsets.all(30.0),
+                    child: const CircularProgressIndicator(),
+                  ),
+                ),
+              ],
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
